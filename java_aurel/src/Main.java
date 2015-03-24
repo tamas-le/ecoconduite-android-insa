7826a9d4debfc73995df5e0e759f162e4ce7efc0
Aurélien, le boss du rap game <aurelien.tamasle@gmail.com>
2015-03-24 17:03:15 +0100

    Ajout d'un début de projet android et modification du java

--- java_aurel/src/Main.java
+++ java_aurel/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Logiciel d'analyse de la trace");
-		System.out.println("Aurélien Tamas Leloup");
+				System.out.println("Aurélien Tamas Leloup");
 		LinkedList<Point> list=CsvAnalyser.generateListe("2014-02-24_18-17-01.csv");
 		Chart chart =new Chart(list);
 		chart.draw();
